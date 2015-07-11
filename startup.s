a1db2c8d44738f12c19248f1881279e6f0a3f494
hak8or <hak8or@gmail.com>
2015-07-11 09:25:06 -0400

    Added better vector table comments

--- startup.s
+++ startup.s
@@ -2,13 +2,17 @@
 .cpu cortex-m3
 .thumb
 
-.word   0x20002000  /* stack top address */
-.word   _start
-.word   hang
-.word   hang
-.word   hang
-.word   hang
-.word   hang
+.word    0x20010000  /* stack top address  */
+.word   _start       /* Reset handler      */
+.word   hang         /* NMI handler        */
+.word   hang         /* Hardfault handler  */
+.word   hang         /* Memfault  handler  */
+.word   hang         /* Busfault handler   */
+.word   hang         /* Undefined ins      */
+/*
+ * Remaining interrupts are not used and therefore not included,
+ * if needed add them here.
+ */
 
 .thumb_func
 .global _start
